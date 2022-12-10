40c9ba39677ef7f401bdd55582b361941593a963
samuel silva <105169384+31samuel@users.noreply.github.com>
2022-12-10 09:51:49 -0300

    commit

---
+++ desafio 3/animaçoes.js/estilo/scripts.js
@@ -0,0 +1,16 @@
+
+function clica()
+{
+ var status=document.getElementById("status");
+ status.innerHTML="Você clicou com o botão do mouse";
+}
+function solta()
+{
+ var status=document.getElementById("status");
+ status.innerHTML="Você soltou o botão do mouse";
+}
+function clica()
+{
+ var status2=document.getElementById("status2");
+ status2.innerHTML="Você clicou com o botão do mouse";
+}
---
+++ desafio 3/desafio 05/script.js
@@ -0,0 +1,3 @@
+window.alert("Minha primeira pagina")
+window.confirm("Esta gostando de java script")
+window.prompt("Qual e seu nome?")
